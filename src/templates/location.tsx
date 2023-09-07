1a2ed90e8bf35cbc8d98ba295482f9a6f73d1aa5
Yext Studio <studio-placeholder@yext.com>
2023-09-07 09:49:53 +0000

    Yext Studio Commit

---
+++ src/templates/location.tsx
@@ -0,0 +1,18 @@
+import { GetPath, TemplateConfig, TemplateProps } from "@yext/pages";
+import Label from "../components/Label";
+
+export const config: TemplateConfig = {
+  stream: {
+    $id: "studio-stream-id",
+    localization: { locales: ["en"], primary: false },
+    filter: { entityTypes: ["Location"] },
+    fields: ["slug"],
+  },
+};
+export const getPath: GetPath<TemplateProps> = ({ document }) => {
+  return `${document.slug}`;
+};
+
+export default function location() {
+  return <Label value="Label" />;
+}
